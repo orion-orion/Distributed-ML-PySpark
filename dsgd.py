0c26c67019579710f8859897a1bb6c53e4bfd03b
orion-orion <orion-orion@foxmail.com>
2022-06-15 20:59:23 +0800

    add dsgd

--- dsgd.py
+++ dsgd.py
@@ -2,152 +2,91 @@
 Descripttion: 
 Version: 1.0
 Author: ZhangHongYu
-Date: 2022-06-14 22:04:48
+Date: 2022-05-26 21:02:38
 LastEditors: ZhangHongYu
-LastEditTime: 2022-06-14 22:05:57
+LastEditTime: 2022-06-15 20:53:34
 '''
-import os
-import sys
+from sklearn.datasets import load_breast_cancer
 import numpy as np
-import time
+from pyspark.sql import SparkSession
+from operator import add
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+n_slices = 3  # Number of Slices
+n_iterations = 300  # Number of iterations
+alpha = 10  # iteration step_size, because gradient sum is divided by minibatch size, it shoulder be larger
+mini_batch_fraction = 0.1 # the fraction of mini batch sample 
+
+def logistic_f(x, w):
+    return 1 / (np.exp(-x.dot(w)) + 1)
 
 
-def readRating(line):
-    line = line.split(",")
-    return [int(line[0]), int(line[1]),float(line[2])]
+def gradient(point: np.ndarray, w: np.ndarray):
+    """ Compute linear regression gradient for a matrix of data points
+    """
+    y = point[-1]    # point label
+    x = point[:-1]   # point coordinate
+    # For each point (x, y), compute gradient function, then sum these up
+    # notice thet we need to compute minibatch size, so return(g, 1)
+    return - (y - logistic_f(x, w)) * x
 
-def genperms(numworkers):
-    return np.random.permutation(numworkers)+1
 
-#function to assign blocks given the number of rows, number of workers, and index of element
-def assignBlockIndex (index, numData, numWorkers):
-    blockSize = numData/numWorkers
-    if(numData % numWorkers != 0): blockSize = blockSize + 1
-    return int(np.floor(index/np.ceil(blockSize)))+1
+if __name__ == "__main__":
 
-def main(numFactors, numWorkers, maxIter, beta, lam):
-    #conf = SparkConf().setAppName('DSGD-MF')
-    #sc = SparkContext(conf=conf)
-    numWorkers = int(numWorkers)
-    maxIter = int(maxIter)
-    numFactors = int(numFactors)
-    beta = float(beta)
-    lam = sc.broadcast(lam)
-    
-    
-    #load data consisting of triples (user, movie, rating)
-    #M = sc.textFile("/FileStore/tables/wdb42krq1465126026181/TrainingRatings.txt").map(readRating).persist()
-    #M = sc.textFile("/FileStore/tables/8j3z7jgi1465112138947/RatingsShuf.txt").map(readRating).persist()
-    M = sc.textFile("/FileStore/tables/7dn7w4ew1465114785307/RatingsShuf.txt").map(readRating).persist()
-    #M = sc.textFile("/FileStore/tables/9lkfxx6x1464574874711/ratings_tiny-993e4.txt").map(readRating)
-  
-    #get basic statistics
-    start = time.time()
-    numRows = M.max(lambda x : x[0])[0] +1
-    numCols = M.max(lambda x : x[1])[1] +1
-    avgRating = M.map(lambda x: x[2]).mean()
-    
-    #compute the scaling factor that the randomly initialized W and H matrices need to be scaled by so that dot(W_0,H_0) results in values that are similar to the average ratings
-    scaleRating = np.sqrt(avgRating / numFactors)
-    
-    tau = 100
-    
-    
-    mseList = []
-    times = []
-    #block the M matrix by assigning blocks so that rows and columns are divided into equal chunks of size numRows/numWorkers and numCols/numWorkers
-    Mblocked = M.keyBy(lambda x: assignBlockIndex(x[0], numRows, numWorkers)).partitionBy(numWorkers)
+    X, y = load_breast_cancer(return_X_y=True)
 
-    #print Mblocked.collect()
-    #initialize the factor matrices and attach with them the count of the number of non-zero elements
-    W = M.map(lambda x: tuple([int(x[0]),1])).reduceByKey(lambda x,y : x+y).map(lambda x: tuple([x[0],tuple([x[1],scaleRating*np.random.rand(1, numFactors).astype(np.float32)])])).persist()
-    H = M.map(lambda x: tuple([int(x[1]),1])).reduceByKey(lambda x,y : x+y).map(lambda x: tuple([x[0],tuple([x[1],scaleRating*np.random.rand(numFactors,1).astype(np.float32)])])).persist()
+    D = X.shape[1]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=0)
+    n_train, n_test = X_train.shape[0], X_test.shape[0]
+
+    spark = SparkSession\
+        .builder\
+        .appName("SGD")\
+        .getOrCreate()
+
+    matrix = np.concatenate(
+        [X_train, np.ones((n_train, 1)), y_train.reshape(-1, 1)], axis=1)
+
+    points = spark.sparkContext.parallelize(matrix, n_slices).cache()
+
+    # Initialize w to a random value
+    w = 2 * np.random.ranf(size=D + 1) - 1
+    print("Initial w: " + str(w))
+
     
-    #main loop through iterations
-    for it in range(maxIter):
-        mse = sc.accumulator(0.0)
-        nUpdates = sc.accumulator(0.0)
-        #broadcast the stepsize
-        stepSize = sc.broadcast(np.power(tau + it, -beta))
-        #generate random strata
-        perms = genperms(numWorkers)
-        #filter the data matrix to have only the entries in the blocks from the current strata
-        Mfilt = Mblocked.filter(lambda x: perms[x[0]-1]==assignBlockIndex(x[1][1],numCols,numWorkers)).persist()
-        #block the W and H matrices using the block number as a key
-        Hblocked = H.keyBy(lambda x: perms[assignBlockIndex(x[0], numRows, numWorkers)-1])
-        Wblocked = W.keyBy(lambda x: assignBlockIndex(x[0], numRows, numWorkers))
-        #group the RDDs together
-        groupRDD = Mfilt.groupWith(Hblocked, Wblocked).partitionBy(numWorkers)
-        Mfilt.unpersist()
+    for t in range(n_iterations):
+        w_br = spark.sparkContext.broadcast(w)
+        print("On iteration %d" % (t + 1))
+
+        (g, mini_batch_size) = points.sample(False, mini_batch_fraction, 42 + t)\
+            .map(lambda point: gradient(point, w_br.value))\
+            .treeAggregate(
+                (0.0, 0),\
+                    seqOp=lambda res, g: (res[0] + g, res[1] + 1),\
+                        combOp=lambda res_1, res_2: (res_1[0] + res_2[0], res_1[1] + res_2[1])
+            )
+        print(g, mini_batch_size)
+        w -= alpha * g/mini_batch_size
         
-        #run SGD on each block
-        WH = groupRDD.mapPartitions(lambda x: SGD(x, stepSize, numFactors,lam, mse, nUpdates,scaleRating)).reduceByKey(lambda x,y: x+y).persist()
+        y_pred = logistic_f(np.concatenate(
+            [X_test, np.ones((n_test, 1))], axis=1), w)
+        pred_label = np.where(y_pred < 0.5, 0, 1)
+        acc = accuracy_score(y_test, pred_label)
+        print("iterations: %d, accuracy: %f" % (t, acc))
 
-        W = WH.filter(lambda x: x[0]=='W').flatMap(lambda x: x[1]).persist()
-        H = WH.filter(lambda x: x[0]=='H').flatMap(lambda x: x[1]).persist()
-        Wvec = W.collect()
-        Hvec = H.collect()
-        mseCur = mse.value / nUpdates.value
-        #check convergence: if just testing scalability comment this section out and run for fixed iterations
-        if len(mseList) > 1:
-          if abs(mseCur[0][0] - mseList[it-1]) < 0.001:
-            mseList.append(mseCur[0][0])
-            times.append(time.time()-start)
-            break
-        mseList.append(mseCur[0][0])
-        times.append(time.time()-start)
-        
+    print("Final w: %s " % w)
+    print("Final acc: %f" % acc)
 
+    spark.stop()
 
-    print times
-    print mseList
-    
-
-
-#function to run SGD on a given block with input iterable of (Vblock, (Wblock, Hbloc))
-def SGD(keyed_iterable, stepSize, numFactors,lam, mse, nUpdates, scaleRating):
-    iterlist = (keyed_iterable.next())
-    Miter = iterlist[1][0]
-    Hiter = iterlist[1][1]
-    Witer = iterlist[1][2]
-    
-    Wdict = {}
-    Hdict = {}
-    
-    Wout = {}
-    Hout = {}
-    
-    #iterate through H and W and create dictionary of elements
-    for h in Hiter:
-        Hdict[h[0]] = h[1]
-    
-    for w in Witer:
-        Wdict[w[0]] = w[1]
-    
-    #iterate through entries of M and compute L2-loss
-    for m in Miter:
-        (i,j,rat) = m
-        
-        if i not in Wdict:
-            Wdict[i] = tuple([i,scaleRating*np.random.rand(1,numFactors).astype(np.float32)])
-        if j not in Hdict:
-            Hdict[j] = tuple([j,scaleRating*np.random.rand(numFactors,1).astype(np.float32)])
-
-        (Nw, Wprev) = Wdict[i]
-        (Nh, Hprev) = Hdict[j]
-
-        delta = -2*(rat - Wprev.dot(Hprev))
-        mse += (rat - Wprev.dot(Hprev))**2
-
-        Wnew = Wprev - stepSize.value*(delta*Hprev.T + (2.0*lam.value/Nh)*Wprev)
-        Hnew = Hprev - stepSize.value*(delta*Wprev.T + (2.0*lam.value/Nw)*Hprev)
-
-        nUpdates += 1
-
-        Wout[i] = tuple([Nw, Wnew])
-        Hout[j] = tuple([Nh, Hnew])
-        
-    return (tuple(['W',Wout.items()]), tuple(['H',Hout.items()]))
-
-
-    #Run by calling main(numFactors, numWorkers, maxIter, beta, lam)
+# Final w: [ 2.47106772e+03  4.53539140e+03  1.49658271e+04  1.49085211e+04
+#   2.49189579e+01  3.86567135e+00 -2.10764497e+01 -9.70220892e+00
+#   4.66690447e+01  1.98304608e+01  5.39929228e-03  3.18391480e+02
+#  -3.33336179e+01 -6.97806573e+03  1.85282484e+00  2.47784246e+00
+#   2.90481836e+00  1.34441817e+00  4.78931649e+00 -4.90105658e-02
+#   2.46075335e+03  5.85553211e+03  1.47565943e+04 -1.50819870e+04
+#   3.35107010e+01  4.44383010e+00 -2.74355495e+01 -5.50744484e+00
+#   6.77728432e+01  2.07616117e+01  3.09770050e+02] 
+# Final acc: 0.912281
