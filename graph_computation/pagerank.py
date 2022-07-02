d75bee6c9f18b42c7ec19fcf8664b8eb02d5d9b6
orion-orion <orion-orion@foxmail.com>
2022-07-02 10:49:32 +0800

    refactor project

---
+++ graph_computation/pagerank.py
@@ -0,0 +1,68 @@
+'''
+Descripttion: 
+Version: 1.0
+Author: ZhangHongYu
+Date: 2022-05-31 14:14:35
+LastEditors: ZhangHongYu
+LastEditTime: 2022-06-03 19:24:53
+'''
+import re
+import sys
+from operator import add
+from typing import Iterable, Tuple
+
+from pyspark.resultiterable import ResultIterable
+from pyspark.sql import SparkSession
+
+n_slices = 3  # Number of Slices
+n_iterations = 10  # Number of iterations
+q = 0.15 #the default value of q is 0.15
+
+def computeContribs(neighbors: ResultIterable[int], rank: float) -> Iterable[Tuple[int, float]]:
+    # Calculates the contribution(rank/num_neighbors) of each vertex, and send it to its neighbours.
+    num_neighbors = len(neighbors)
+    for vertex in neighbors:
+        yield (vertex, rank / num_neighbors)
+
+if __name__ == "__main__":
+    # Initialize the spark context.
+    spark = SparkSession\
+        .builder\
+        .appName("PythonPageRank")\
+        .getOrCreate()
+
+    # link: (source_id, dest_id)
+    links = spark.sparkContext.parallelize(
+        [(1, 2), (1, 3), (2, 3), (3, 1)],
+        n_slices
+    )                       
+
+    # drop duplicate links and convert links to an adjacency list.
+    adj_list = links.distinct().groupByKey().cache()
+
+    # count the number of vertexes
+    n_vertexes = adj_list.count()
+
+    # init the rank of each vertex, the default is 1.0/n_vertexes
+    ranks = adj_list.map(lambda vertex_neighbors: (vertex_neighbors[0], 1.0/n_vertexes))
+
+    # Calculates and updates vertex ranks continuously using PageRank algorithm.
+    for t in range(n_iterations):
+        # Calculates the contribution(rank/num_neighbors) of each vertex, and send it to its neighbours.
+        contribs = adj_list.join(ranks).flatMap(lambda vertex_neighbors_rank: computeContribs(
+            vertex_neighbors_rank[1][0], vertex_neighbors_rank[1][1]  # type: ignore[arg-type]
+        ))
+
+        # Re-calculates rank of each vertex based on the contributions it received
+        ranks = contribs.reduceByKey(add).mapValues(lambda rank: q/n_vertexes + (1 - q)*rank)
+
+    # Collects all ranks of vertexs and dump them to console.
+    for (vertex, rank) in ranks.collect():
+        print("%s has rank: %s." % (vertex, rank))
+
+    spark.stop()
+    
+    
+# 1 has rank: 0.38891305880091237.  
+# 2 has rank: 0.214416470596171.
+# 3 has rank: 0.3966704706029163.
---
+++ graph_computation/transitive_closure.py
@@ -0,0 +1,44 @@
+'''
+Descripttion: 
+Version: 1.0
+Author: ZhangHongYu
+Date: 2022-07-01 22:04:00
+LastEditors: ZhangHongYu
+LastEditTime: 2022-07-02 10:17:32
+'''
+from pyspark.sql import SparkSession
+n_slices = 2  # Number of Slices
+
+if __name__ == "__main__":
+    spark = SparkSession\
+        .builder\
+        .appName("TransitiveClosure")\
+        .getOrCreate()
+     
+    paths = spark.sparkContext.parallelize([(1, 2), (1, 3), (2, 3), (3, 1)], n_slices).cache()
+
+    # Linear transitive closure: each round grows paths by one edge,
+    # by joining the the already-discovered paths with graph's edges. 
+    # e.g. join the path (y, z) from the paths with the edge (x, y) from
+    # the graph to obtain the new path (x, z).
+    
+
+    # The edges are stored in reversed order because they are about to be joined.
+    edges = paths.map(lambda x_y: (x_y[1], x_y[0]))
+
+    old_cnt = 0
+    next_cnt = paths.count()
+    while True:
+        old_cnt = next_cnt
+        # Perform the join, obtaining an RDD of (y, (z, x)) pairs,
+        # then map the result to obtain the new (x, z) paths.
+        new_paths = paths.join(edges).map(lambda vertexes: (vertexes[1][1], vertexes[1][0]))
+        # union new paths
+        paths = paths.union(new_paths).distinct().cache()
+        next_cnt = paths.count()
+        if next_cnt == old_cnt:
+            break
+
+    print("The original graph has %i paths" % paths.count())
+
+    spark.stop()
